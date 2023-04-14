41bbca3d116f7b49bd1dd70087c6f4bf25c57d53
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-04-14 22:44:16 +0300

    [#25] create and setup project (#35)
    * Front application init
    
    * setup webpack, eslint and prettier
    
    * Fix prettier settings
    
    * Adjust webpack settings
    
    * Rearange project structure

---
+++ src/Webapp/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+const devtool = mode === 'production' ? false : 'inline-source-map'
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode,
+  devtool,
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './public/favicon.ico',
+      manifest: 'public/manifest.json',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              noEmit: false,
+            },
+          },
+        },
+      },
+      {
+        test: /\.(html)$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico|json)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+  },
+}
