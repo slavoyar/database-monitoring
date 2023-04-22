ac709a1dfc6c5f5fb56435f29af518bc130a27f3
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-04-22 20:33:27 +0300

    add logo

--- src/Webapp/src/components/common/Navbar.tsx
+++ src/Webapp/src/components/common/Navbar.tsx
@@ -1,40 +1,51 @@
 import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { DashboardOutlined, LineChartOutlined, SettingOutlined } from '@ant-design/icons'
 import { Layout, Menu, Select } from 'antd'
 import 'css/Navbar.css'
 
 const { Header } = Layout
 
-const Navbar: FC = () => (
-  <Header className='header'>
-    <div className='logo' />
-    <Menu
-      theme='dark'
-      className='navbar-menu'
-      mode='horizontal'
-      defaultSelectedKeys={['/dashboard']}
-    >
-      <Menu.Item key='/dashboard'>
-        <Link to='/dashboard'>
-          <DashboardOutlined />
-          Обзорная панель
+const Navbar: FC = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const onLogoClick = () => {
+    if (location.pathname !== '/dashboard') {
+      navigate('/dashboard')
+    }
+  }
+  return (
+    <Header className='header'>
+      <button type='button' className='logo' onClick={onLogoClick}>
+        МИАУ
+      </button>
+      <Menu
+        theme='dark'
+        className='navbar-menu'
+        mode='horizontal'
+        defaultSelectedKeys={['/dashboard']}
+      >
+        <Menu.Item key='/dashboard'>
+          <Link to='/dashboard'>
+            <DashboardOutlined />
+            Обзорная панель
+          </Link>
+        </Menu.Item>
+        <Menu.Item key='stats'>
+          <Link to='/dashboard'>
+            <LineChartOutlined />
+            Статистика
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <div className='navbar-settings'>
+        <Select placeholder='Workspace' className='navbar-workspace-select' />
+        <Link to='/admin'>
+          <SettingOutlined />
         </Link>
-      </Menu.Item>
-      <Menu.Item key='stats'>
-        <Link to='/dashboard'>
-          <LineChartOutlined />
-          Статистика
-        </Link>
-      </Menu.Item>
-    </Menu>
-    <div className='navbar-settings'>
-      <Select className='navbar-workspace-select' />
-      <Link to='/admin'>
-        <SettingOutlined />
-      </Link>
-    </div>
-  </Header>
-)
+      </div>
+    </Header>
+  )
+}
 
 export default Navbar
