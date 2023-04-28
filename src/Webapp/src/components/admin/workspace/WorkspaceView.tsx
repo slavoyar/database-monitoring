50150db1ebd4ad270b848c007ee86c952611e5fc
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-04-29 00:06:08 +0300

    refactor structure, add workspaceview component

---
+++ src/Webapp/src/components/admin/workspace/WorkspaceView.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+import WorkspaceTable from './WorkspaceTable'
+
+const WorkspaceView: FC = () => {
+  return (
+    <>
+      <h1>Рабочие пространства</h1>
+      <WorkspaceTable />
+    </>
+  )
+}
+
+export default WorkspaceView
