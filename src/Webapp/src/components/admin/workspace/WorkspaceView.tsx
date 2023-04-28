9ff39db086ef4b4dca171df89a209856a418f913
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-04-29 01:00:27 +0300

    add button to add and delete entries

--- src/Webapp/src/components/admin/workspace/WorkspaceView.tsx
+++ src/Webapp/src/components/admin/workspace/WorkspaceView.tsx
@@ -2,13 +2,11 @@ import { FC } from 'react'
 
 import WorkspaceTable from './WorkspaceTable'
 
-const WorkspaceView: FC = () => {
-  return (
-    <>
-      <h1>Рабочие пространства</h1>
-      <WorkspaceTable />
-    </>
-  )
-}
+const WorkspaceView: FC = () => (
+  <>
+    <h1>Рабочие пространства</h1>
+    <WorkspaceTable />
+  </>
+)
 
 export default WorkspaceView
