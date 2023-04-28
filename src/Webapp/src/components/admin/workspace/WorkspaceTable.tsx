c0c8be22be2335aaae9df1870ec3af4d846e4445
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-04-29 01:00:27 +0300

    add button to add and delete entries

--- src/Webapp/src/components/admin/workspace/WorkspaceTable.tsx
+++ src/Webapp/src/components/admin/workspace/WorkspaceTable.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
-import { Table } from 'antd'
+import React, { FC, useState } from 'react'
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons'
+import { Button, Table } from 'antd'
 
 enum WorkspaceTableColumn {
   NAME = 'name',
@@ -7,7 +8,14 @@ enum WorkspaceTableColumn {
   SERVERS = 'servers',
 }
 
-const MOCK_DATA = [
+interface WorkspaceTableData {
+  key: string
+  [WorkspaceTableColumn.NAME]: string
+  [WorkspaceTableColumn.USERS]: string
+  [WorkspaceTableColumn.SERVERS]: string
+}
+
+const MOCK_DATA: WorkspaceTableData[] = [
   {
     key: '1',
     [WorkspaceTableColumn.NAME]: 'Workspace1',
@@ -46,6 +54,58 @@ const columns = [
   },
 ]
 
-const WorkspaceTable: FC = () => <Table dataSource={MOCK_DATA} columns={columns} />
+const WorkspaceTable: FC = () => {
+  const [data, setData] = useState<WorkspaceTableData[]>(MOCK_DATA)
+  const [deleteDisabled, setDeleteDisabled] = useState(true)
+  const [selectedRows, setSelectedRows] = useState<React.Key[]>([])
+
+  const onAddClick = () => {
+    setData((prevData) => {
+      const newKey = String(Number(prevData[prevData.length - 1].key) + 1)
+      return [
+        ...prevData,
+        {
+          key: newKey,
+          [WorkspaceTableColumn.NAME]: 'Workspace2',
+          [WorkspaceTableColumn.USERS]: 'User1',
+          [WorkspaceTableColumn.SERVERS]: 'Server1, Server2',
+        },
+      ]
+    })
+  }
+
+  const onDeleteClick = () => {
+    const newData = data.filter((item) => !selectedRows.includes(item.key))
+    setSelectedRows([])
+    setData(newData)
+    setDeleteDisabled(true)
+  }
+
+  const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+    setDeleteDisabled(!newSelectedRowKeys.length)
+    setSelectedRows(newSelectedRowKeys)
+  }
+
+  const rowSelection = {
+    selectedRows,
+    onChange: onSelectChange,
+  }
+
+  return (
+    <>
+      <Button.Group>
+        <Button icon={<PlusOutlined />} onClick={onAddClick} />
+        <Button
+          icon={<DeleteOutlined />}
+          disabled={deleteDisabled}
+          type='primary'
+          danger
+          onClick={onDeleteClick}
+        />
+      </Button.Group>
+      <Table bordered dataSource={data} columns={columns} rowSelection={rowSelection} />
+    </>
+  )
+}
 
 export default WorkspaceTable
