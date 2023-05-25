612c0472f5918d612bf09b1a28ca59875585fef6
Дмитрий Брилько <DimaBrilko@gmail.com>
2023-05-25 19:24:51 +0300

    Added in set service

--- src/Services/Agregation/Services/Implementations/LogSetService.cs
+++ src/Services/Agregation/Services/Implementations/LogSetService.cs
@@ -8,12 +8,16 @@ namespace MIAUDataBase.Services.Implementations
 {
     public class LogSetService : AbstractSetService<LogDto, Log>, ILogSetService
     {
+        new private readonly ILogRepository repository;
         public LogSetService(ILogRepository repository, IMapper mapper) : base(repository, mapper)
         {
+            this.repository = repository;
         }
         public async Task<List<LogDto>> GetAllForServerAsync(string serverId, int itemsPerPage, int page)
         {
-            repository.Get
+            var entities = await repository.GetAllForServerAsync(serverId, itemsPerPage, page);
+            var dtos = mapper.Map<ICollection<LogDto>>(entities);
+            return dtos.ToList();
         }        
     }
 }
