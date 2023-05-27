1c3f6101ff0422aef3e3b594ad8d5fd27c9e1f87
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-05-27 13:31:48 +0300

    remove common services. Add urls to appsettings

---
+++ src/ApiGateways/WebAggregator/Extensions/CommonExtensions.cs
@@ -0,0 +1,37 @@
+using System.IdentityModel.Tokens.Jwt;
+internal static class CommonExtensions
+{
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+  {
+    services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+    return services;
+  }
+
+  public static IServiceCollection AddDefaultAuthentication(this IServiceCollection services, IConfiguration configuration)
+  {
+    var identitySection = configuration.GetSection("Identity");
+
+    if (!identitySection.Exists())
+    {
+      // No identity section, so no authentication
+      return services;
+    }
+
+    JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+    services.AddAuthentication().AddJwtBearer(options =>
+    {
+      var identityUrl = identitySection.GetRequiredValue("Url");
+      var audience = identitySection.GetRequiredValue("Audience");
+
+      options.Authority = identityUrl;
+      options.RequireHttpsMetadata = false;
+      options.Audience = audience;
+      options.TokenValidationParameters.ValidateAudience = false;
+    });
+
+    return services;
+  }
+
+  
+}
--- src/ApiGateways/WebAggregator/Program.cs
+++ src/ApiGateways/WebAggregator/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
