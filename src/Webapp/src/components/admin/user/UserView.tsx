1191bc4b79ab5e0b58366b480e96fed7824a3da2
Yaroslav Zaprudskii <slavoyarmc@gmail.com>
2023-05-28 14:19:12 +0300

    Add User table component

---
+++ src/Webapp/src/components/admin/user/UserView.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+import UserTable from './UserTable'
+
+const UserView: FC = () => (
+  <>
+    <h1>Пользователи</h1>
+    <UserTable />
+  </>
+)
+
+export default UserView
