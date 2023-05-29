1233bab1309450562410c85eb6fbe1fe0211a3bf
Batiskaaaf <94691413+Batiskaaaf@users.noreply.github.com>
2023-05-29 21:30:28 +0200

    104 workspace service (#117)
    * Add workspace service interface
    
    * Add workspace service interface
    
    ---------
    
    Co-authored-by: Yaroslav Zaprudskii <slavoyarmc@gmail.com>

--- src/Services/Notification/Notification.WebApi/Program.cs
+++ src/Services/Notification/Notification.WebApi/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -12,7 +13,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<MailConfiguration>(builder.Configuration.GetSection(nameof(MailConfiguration)));
+builder.Services.Configure<WorkspaceConfiguration>(builder.Configuration.GetSection(nameof(WorkspaceConfiguration)));
+
 builder.Services.AddScoped<IMailService, MailService>();
 builder.Services.AddScoped<IRepository<MailEntity>, EfRepository<MailEntity>>();
 builder.Services.AddScoped<IRepository<ErrorSending>, EfRepository<ErrorSending>>();
---
+++ src/Services/Notification/Notification.WebApi/Services/WorkspaceService.cs
@@ -0,0 +1,9 @@
+namespace DatabaseMonitoring.Services.Notification.WebApi.Services;
+
+public class WorkspaceService : IWorkspaceService
+{
+    public Task<Guid> GetServerWorkspaces(Guid serverId)
+    {
+        throw new NotImplementedException();
+    }
+}
