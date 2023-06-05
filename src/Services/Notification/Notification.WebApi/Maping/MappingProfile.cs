890289b7a1b4a055da365f6e73d73a41c9c26cd8
batiskaaaf <batiskaf1@outlook.com>
2023-06-05 19:12:18 +0200

    Add endpoints

--- src/Services/Notification/Notification.WebApi/Maping/MappingProfile.cs
+++ src/Services/Notification/Notification.WebApi/Maping/MappingProfile.cs
@@ -5,5 +5,7 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<SendEmailRequest, MailData>();
+
+        CreateMap<NotificationDto, UnreadNotification>();
     }
 }
