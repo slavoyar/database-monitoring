7a6a1a6aff39b9320e6fe3ff2c18d158db32c02f
Дмитрий Брилько <DimaBrilko@gmail.com>
2023-06-08 20:30:47 +0300

    Добавил в ServerPatientController нужные эндпоинты.
    Эндпоинты:
        api/Aggregation/Server/{ShortServersRequest} GET (Получить краткую информацию о серверах)
    
        api/Aggregation/Server/{ServerGetRequest} GET (Получить полную информацию о сервере)
        api/Aggregation/Server/{ServerUpdateRequest} PUT (Обновить информацию о сервере)
    
        api/Aggregation/Server/{ServerId} DELETE (Удалить)
        api/Aggregation/Server/{ServerCreateRequest} POST (Создать)

--- src/Services/Agregation/Controllers/ServerPatientController.cs
+++ src/Services/Agregation/Controllers/ServerPatientController.cs
@@ -6,20 +6,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MIAUDataBase.Controllers
 {
-    public class ServerPatientController :
-        AbstractController<
-            ServerPatientCreateModel, ServerPatientEditModel, ServerPatientViewModel, ServerPatientDto>
+    [Route("[controller]")]
+    public class ServerPatientController
     {
         protected readonly ILogSetService logSetService;
         protected readonly IServerPatientSetService serverPatientSetService;
+        protected readonly IMapper mapper;
         public ServerPatientController( ILogSetService logSetService,
-            IServerPatientSetService setService, IMapper mapper) : base(setService, mapper)
+            IServerPatientSetService setService, IMapper mapper)
         {
             this.logSetService = logSetService;
             serverPatientSetService = setService;
+            this.mapper = mapper;
         }
 
-        [HttpGet("shortList/{page}/{itemsPerPage}")]
+        /// <summary>
+        /// Получить список полной информации о серверах пациентах в пагинированном виде.
+        /// </summary>
+        /// <param name="page">Номер страницы.</param>
+        /// <param name="itemsPerPage">Количесвто элементов на странице.</param>
+        /// <returns>Возвращает itemsPerPage серверов пациентов с полной информацией о них.</returns>
+        [HttpGet("list/{page}/{itemsPerPage}")]
+        public async Task<IResult> GetPaged(int page, int itemsPerPage)
+        {
+            if (page <= 0 || itemsPerPage <= 0) return Results.ValidationProblem(new Dictionary<string, string[]>() {
+                    { "page or items per page less or equal then 0" , new string[]{ "Enter correct numbers" }  },
+                });
+            var dtoPage = await serverPatientSetService.GetPagedAsync(page, itemsPerPage);
+            var viewModelPage = mapper.Map<ICollection<ServerPatientViewModel>>(dtoPage);
+            return Results.Ok(viewModelPage);
+        }
+
+        /// <summary>
+        /// Получить список серверов с кратким описанием в пагинированном виде
+        /// </summary>
+        /// <param name="page">Номер страницы</param>
+        /// <param name="itemsPerPage">Количество серверов на странице</param>
+        /// <returns>Возвращвет список серверов с краткой информацией</returns>
+        [HttpGet("Aggregation/Server/ShortServersRequest/{page}/{itemsPerPage}")]
         public async Task<IResult> GetShortListByPage(int page, int itemsPerPage)
         {
             if (page <= 0 || itemsPerPage <= 0) return Results.ValidationProblem(new Dictionary<string, string[]>() {
@@ -28,6 +52,64 @@ namespace MIAUDataBase.Controllers
             var dtoPage = await serverPatientSetService.GetShortServerPatientsPaged(page, itemsPerPage);
             var viewModelPage = mapper.Map<ICollection<ServerPatientShortViewModel>>(dtoPage);
             return Results.Ok(viewModelPage);
+        }
+
+        /// <summary>
+        /// Создать новый сервер пациент.
+        /// </summary>
+        /// <param name="ServerCreateRequest">Информация о сервере пациенте для создания.</param>
+        /// <returns>Возвращает информацию о сервере с его идентификатором.</returns>
+        [HttpPost("Aggregation/Server/{ServerCreateRequest}")]
+        public async Task<IResult> Create(ServerPatientCreateModel ServerCreateRequest)
+        {
+            var dto = mapper.Map<ServerPatientDto>(ServerCreateRequest);
+            var retObj = await serverPatientSetService.AddAsync(dto);
+            var retModel = mapper.Map<ServerPatientViewModel>(retObj);
+            return Results.Created("Not uri", retModel);
+        }
+
+        /// <summary>
+        /// Удалить сервер пациент.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        /// <returns>Возвращает 200, при удалении, 404, если сервер с эти ид не найден.</returns>
+        [HttpDelete("Aggregation/Server/{id}")]
+        public async Task<IResult> DeleteById(Guid id)
+        {
+            var result = await serverPatientSetService.TryDeleteAsync(id);
+            return result ? Results.Ok() : Results.NotFound();
+        }
+
+        /// <summary>
+        /// Получить полную информацию о сервере по идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        /// <returns>Возвращает информацию о сервере.</returns>
+        [HttpGet("Aggregation/Server/{id}")]
+        public async Task<IResult> GetById(Guid id)
+        {
+            var dto = await serverPatientSetService.GetAsync(id);
+            if (dto == null) {
+                return Results.NotFound();
+            }
+            var view = mapper.Map<ServerPatientViewModel>(dto);
+            return Results.Ok(view);
+        }
+
+        /// <summary>
+        /// Обновить информацию о сервере.
+        /// </summary>
+        /// <param name="ServerUpdateRequest"></param>
+        /// <returns></returns>
+        [HttpPut("Aggregation/Server/{ServerUpdateRequest}")]
+        public async Task<IResult> Edit(ServerPatientEditModel ServerUpdateRequest)
+        {
+            var dto = mapper.Map<ServerPatientDto>(ServerUpdateRequest);
+            var result = await serverPatientSetService.TryUpdateAsync(dto);
+            if (result)
+                return Results.Ok();
+            else
+                return Results.NotFound();
         }
     }
 }
