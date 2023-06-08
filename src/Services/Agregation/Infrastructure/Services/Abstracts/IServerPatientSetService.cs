abde1b20c12fd6c5c64a01a03c6e13b92a8aaaa7
Дмитрий Брилько <DimaBrilko@gmail.com>
2023-06-08 22:18:05 +0300

    Удалил лишние наследования в IServerPatientSetService и в ServerPatientSetService

--- src/Services/Agregation/Infrastructure/Services/Abstracts/IServerPatientSetService.cs
+++ src/Services/Agregation/Infrastructure/Services/Abstracts/IServerPatientSetService.cs
@@ -6,8 +6,14 @@ namespace Agregation.Infrastructure.Services.Abstracts
     /// <summary>
     /// Нужен для DI. 
     /// </summary>
-    public interface IServerPatientSetService : ISetService<ServerPatientDto>
+    public interface IServerPatientSetService
     {
         public Task<List<ShortServerPatientDto>> GetShortServerPatientsPaged(int page, int itemsPerPage);
+        public Task<ICollection<ServerPatientDto>> GetPagedAsync(int page, int itemsPerPage);
+        public Task<ServerPatientDto> AddAsync(ServerPatientDto dto);
+        public Task<bool> TryDeleteAsync(Guid id);
+        public Task<ServerPatientDto?> GetAsync(Guid id);
+        public Task<bool> TryUpdateAsync(ServerPatientDto dto);
+
     }
 }
--- src/Services/Agregation/Infrastructure/Services/Implementations/ServerPatientSetService.cs
+++ src/Services/Agregation/Infrastructure/Services/Implementations/ServerPatientSetService.cs
@@ -6,15 +6,17 @@ using AutoMapper;
 
 namespace Agregation.Infrastructure.Services.Implementations
 {
-    public class ServerPatientSetService :
-        AbstractSetService<ServerPatientDto, ServerPatient>, IServerPatientSetService
+    public class ServerPatientSetService : IServerPatientSetService
     {
         protected readonly ILogRepository logRepository;
         protected readonly IServerPatientRepository serverPatientRepository;
-        public ServerPatientSetService(ILogRepository _logRepository, IServerPatientRepository repository, IMapper mapper) : base(repository, mapper)
+        protected readonly IMapper mapper;
+        public ServerPatientSetService(
+            ILogRepository _logRepository, IServerPatientRepository repository, IMapper mapper)
         {
             logRepository = _logRepository;
             serverPatientRepository = repository;   
+            this.mapper = mapper;
         }
 
         public async Task<List<ShortServerPatientDto>> GetShortServerPatientsPaged(int page, int itemsPerPage)
@@ -26,6 +28,47 @@ namespace Agregation.Infrastructure.Services.Implementations
                 shortServersPatients[i].CountOfLogs = count;
             }
             return shortServersPatients;
+        }
+
+        public async Task<ICollection<ServerPatientDto>> GetPagedAsync(int page, int itemsPerPage)
+        {
+            var entities = await serverPatientRepository.GetPagedAsync(page, itemsPerPage);
+            return mapper.Map<ICollection<ServerPatientDto>>(entities);
+        }
+
+        public async Task<ServerPatientDto> AddAsync(ServerPatientDto dto)
+        {
+            var entity = mapper.Map<ServerPatient>(dto);
+            var entityToReturn = await serverPatientRepository.AddAsync(entity);
+            await serverPatientRepository.SaveChangesAsync();
+            var dtoToReturn = mapper.Map<ServerPatientDto>(entityToReturn);
+            return dtoToReturn;
+        }
+
+        public async Task<bool> TryDeleteAsync(Guid id)
+        {
+            return await Task.Run(() => {
+                var isSuccess = serverPatientRepository.TryDelete(id);
+                serverPatientRepository.SaveChanges();
+                return isSuccess;
+            });
+        }
+
+        public async Task<ServerPatientDto?> GetAsync(Guid id)
+        {
+            var entity = await serverPatientRepository.GetAsync(id);
+            var dto = mapper.Map<ServerPatientDto>(entity);
+            return dto;
+        }
+
+        public async Task<bool> TryUpdateAsync(ServerPatientDto dto)
+        {
+            var entity = mapper.Map<ServerPatient>(dto);
+            return await Task.Run(() => {
+                var isSuccess = serverPatientRepository.TryUpdate(entity);
+                serverPatientRepository.SaveChanges();
+                return isSuccess;
+            });
         }
     }
 }
