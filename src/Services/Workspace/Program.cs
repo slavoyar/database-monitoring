e9f1a9bce658ef69bdc0d1785fba4f45ecd64c5f
Evgen Kost <kost-evgen@mail.ru>
2023-07-02 18:30:17 +0300

    Обновлены connection strings
    1. Добавлены connection string в docker-compose
    2. connection strings микросервисов возвращены на локальные значения 
    2. Добавлены локальные файлы rabbitMq в git ignore

--- src/Services/Workspace/Program.cs
+++ src/Services/Workspace/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-//await UpdateDatabaseAsync(app);
+await UpdateDatabaseAsync(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,11 +26,11 @@ app.MapControllers();
 app.Run();
 
 
-//async Task UpdateDatabaseAsync(WebApplication app)
-//{
-//    using ( var scope = app.Services.CreateScope() )
-//    {
-//        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//        await context.Database.MigrateAsync();
-//    }
-//}
+async Task UpdateDatabaseAsync(WebApplication app)
+{
+    using ( var scope = app.Services.CreateScope() )
+    {
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await context.Database.MigrateAsync();
+    }
+}
