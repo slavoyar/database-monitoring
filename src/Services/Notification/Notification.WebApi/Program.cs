ffac622c8ffcc5149a37015e6a0dfbaf61fb1fd6
Evgen-Kost <35523236+Evgen-Kost@users.noreply.github.com>
2023-07-06 20:41:03 +0300

    149 [ПРОЕКТ] Архитектура Docker (#161)
    * 1. Добавлены docker файлов для каждого проекта
    2. Добавлен docker-compose для агрегации вызовов всех docker файлов
    3. Изменены connection string для подключения к docker контейнеру
    
    * 1. Добавлены connection string в docker-compose
    2. connection strings микросервисов возвращены на локальные значения 
    2. Добавлены локальные файлы rabbitMq в git ignore
    
    * Обновлены connection strings
    
    1. Добавлены connection string в docker-compose
    2. connection strings микросервисов возвращены на локальные значения 
    2. Добавлены локальные файлы rabbitMq в git ignore
    
    * add docker link for Notification events
    
    after Task 160
    
    * Add img files for wiki
    
    * Updating Notification Docker file
    
    * Disable slash (for Linux)

--- src/Services/Notification/Notification.WebApi/Program.cs
+++ src/Services/Notification/Notification.WebApi/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.Configure<MailConfiguration>(builder.Configuration.GetSection(nameof(MailConfiguration)));
 builder.Services.Configure<MongoDbConfiguration>(builder.Configuration.GetSection(nameof(MongoDbConfiguration)));
 
-
 builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
 builder.Services.AddScoped<INotificationService, NotificationService>();
 
@@ -27,7 +26,7 @@ var app = builder.Build();
 app.Services.EventBusSubscribeToEvents();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if ( app.Environment.IsDevelopment() )
 {
     app.UseSwagger();
     app.UseSwaggerUI();
