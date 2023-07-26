2bde083cd3ed22a4a071086482371b9f4b050d2e
Evgen-Kost <35523236+Evgen-Kost@users.noreply.github.com>
2023-07-26 22:11:23 +0300

    Add project for TestPatient (#173)
    * 1) Add project for TestPatient
    2) Upgrade docker-compose to shorter names
    3) Upgrade docker-compose to run testPatient
    4) To collect logs you need to run controller method (FireAndForget Job or Recurring Job)
    
    * Add SendedStatus, new docker-compose

---
+++ src/Services/TestPatient/Migrations/20230725155918_LogsModel-Delete-ReceivedAt-Add-Sended.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestPatient.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogsModelDeleteReceivedAtAddSended : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReceivedAt",
+                table: "PatientLogs");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Sended",
+                table: "PatientLogs",
+                type: "smallint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sended",
+                table: "PatientLogs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReceivedAt",
+                table: "PatientLogs",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
