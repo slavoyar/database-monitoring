5195c4241144ca550de6f18eccbecfe64098d55f
Evgen Kost <kost-evgen@mail.ru>
2023-08-08 07:28:32 +0300

    Add migrations of test data

--- src/Services/Agregation/Migrations/20230807180340_add-test-data.Designer.cs
+++ src/Services/Agregation/Migrations/20230807180340_add-test-data.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Agregation.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Agregation.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230807180340_add-test-data")]
+    partial class addtestdata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -112,7 +115,7 @@ namespace Agregation.Migrations
                             ConnectionStatus = true,
                             IconId = "1",
                             IdAddress = "testpatient-1",
-                            LastSuccessLog = "2023-08-07 21:08:53",
+                            LastSuccessLog = "2023-08-07 21:03:40",
                             Name = "testpatient1",
                             PingStatus = true,
                             Status = "Working"
@@ -123,7 +126,7 @@ namespace Agregation.Migrations
                             ConnectionStatus = true,
                             IconId = "1",
                             IdAddress = "testpatient-2",
-                            LastSuccessLog = "2023-08-07 21:08:53",
+                            LastSuccessLog = "2023-08-07 21:03:40",
                             Name = "testpatient2",
                             PingStatus = true,
                             Status = "Working"
@@ -134,7 +137,7 @@ namespace Agregation.Migrations
                             ConnectionStatus = true,
                             IconId = "1",
                             IdAddress = "testpatient-3",
-                            LastSuccessLog = "2023-08-07 21:08:53",
+                            LastSuccessLog = "2023-08-07 21:03:40",
                             Name = "testpatient3",
                             PingStatus = true,
                             Status = "Working"
---
+++ src/Services/Agregation/Migrations/20230807180723_add-test-data2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agregation.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtestdata2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ServerPatients",
+                keyColumn: "Id",
+                keyValue: new Guid("8d8a6029-676a-4e09-91c5-32c56602f67f"),
+                column: "LastSuccessLog",
+                value: "2023-08-07 21:07:23");
+
+            migrationBuilder.UpdateData(
+                table: "ServerPatients",
+                keyColumn: "Id",
+                keyValue: new Guid("d13920a2-4961-43cc-bd22-12187b19f512"),
+                column: "LastSuccessLog",
+                value: "2023-08-07 21:07:23");
+
+            migrationBuilder.UpdateData(
+                table: "ServerPatients",
+                keyColumn: "Id",
+                keyValue: new Guid("d69cd87f-1f08-4b12-af16-980b003cdc5f"),
+                column: "LastSuccessLog",
+                value: "2023-08-07 21:07:23");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ServerPatients",
+                keyColumn: "Id",
+                keyValue: new Guid("8d8a6029-676a-4e09-91c5-32c56602f67f"),
+                column: "LastSuccessLog",
+                value: "2023-08-07 21:04:41");
+
+            migrationBuilder.UpdateData(
+                table: "ServerPatients",
+                keyColumn: "Id",
+                keyValue: new Guid("d13920a2-4961-43cc-bd22-12187b19f512"),
+                column: "LastSuccessLog",
+                value: "2023-08-07 21:04:41");
+
+            migrationBuilder.UpdateData(
+                table: "ServerPatients",
+                keyColumn: "Id",
+                keyValue: new Guid("d69cd87f-1f08-4b12-af16-980b003cdc5f"),
+                column: "LastSuccessLog",
+                value: "2023-08-07 21:04:41");
+        }
+    }
+}
