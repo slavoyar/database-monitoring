cfa697c22f0013761e0180296d326ba029929417
batiskaaaf <batiskaf1@outlook.com>
2023-08-10 22:06:07 +0200

    Refactored viewmodels

--- src/Services/Notification/Notification.WebApi/ViewModels/MarkNotificationAsReadRequest.cs
+++ src/Services/Notification/Notification.WebApi/ViewModels/MarkNotificationAsReadRequest.cs
@@ -4,5 +4,7 @@ public class MarkNotificationAsReadRequest
 {   
     [Required]
     public Guid UserId { get; set; }
+    
+    [Required]
     public IEnumerable<string> NotificationsId { get; set; }
 }
