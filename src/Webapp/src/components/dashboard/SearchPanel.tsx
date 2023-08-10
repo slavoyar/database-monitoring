e4e5928179a1e131b9d62c25867c2727f3d679ca
Дмитрий Брилько <DimaBrilko@gmail.com>
2023-08-10 21:06:41 +0300

    q

--- src/Webapp/src/components/dashboard/SearchPanel.tsx
+++ src/Webapp/src/components/dashboard/SearchPanel.tsx
@@ -1,16 +1,26 @@
 // import { FC } from 'react';
 
 
+function Querty() {
+    return (
+        <div>
+            <p>Ыт ыз нот квэрты</p>
+        </div>
+    );
+}
 
 export default function SearchPanel() {
+    let q=1;
     return(
-        <>
+        <div>
+            <Querty></Querty>
             <button type="button">
                 Отсортировать по названию
             </button>
+            <p>{q}</p>
             <button type="button">
                 Отсортировать по адресу
             </button>
-        </>
+        </div>
     );
 }
